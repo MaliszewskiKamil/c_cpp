797632c9892362b634e8b58ab1bf318cf0cbcdcd
Kamil Maliszewski <Kamil.Maliszewski@hva.nl>
2022-01-19 00:42:03 +0100

    init of project, basic setup

---
+++ pass_generator/password_generator.cpp
@@ -0,0 +1,36 @@
+#include <random>
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int password_length;
+const char* const small_characters[24] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "w", "x", "y", "z"};
+const char* const capital_characters[24] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "W", "X", "Y", "Z"};
+const int min_range = 0;
+const int max_range = 24;
+int random_integer;
+char password;
+
+int main(){
+  cout << "Choose a length of a password: ";
+  cin >> password_length;
+  password = new char* const [password_length];
+
+  
+  srand(time(NULL));
+
+  for(int i = 0; i<password_length; i++){
+
+    random_integer = rand() % max_range + 1; 
+    password += small_characters[random_integer];
+  }
+
+  cout << password;
+ 
+
+
+  return 0;
+
+}
